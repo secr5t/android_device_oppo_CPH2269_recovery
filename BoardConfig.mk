8273be29c5db1df72bb518577cca1f71ae266123
secr5t <rottenman02@gmail.com>
2026-08-07 14:55:21 +0000

    add brightness config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,3 +106,6 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 2500
+TW_MAX_BRIGHTNESS := 4095
